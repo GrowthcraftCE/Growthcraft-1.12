4aa2b3b1e6c38775eb5f06cb9d45ed292ba63ea6
Alatyami <alatyami@gmail.com>
2017-07-22 13:22:45 -0400

    Growthcraft-1.11-3 Fishtrap now has Input and Output Inventories. Hoppers will now only pull from the output and push to the input. Thanks to Kisutora debugging, Fishtrap now has a tooltip and Light Blue Crowbar now has a vaild name.

--- src/main/java/growthcraft/fishtrap/Reference.java
+++ src/main/java/growthcraft/fishtrap/Reference.java
@@ -3,7 +3,7 @@ package growthcraft.fishtrap;
 public class Reference {
     public static final String MODID = "growthcraft_fishtrap";
     public static final String NAME = "Growthcraft Fishtrap";
-    public static final String VERSION = "3.0.0-alpha";
+    public static final String VERSION = "3.0.0.1";
 
 	public static final String CLIENT_PROXY_CLASS = "growthcraft.fishtrap.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "growthcraft.fishtrap.proxy.CommonProxy";
