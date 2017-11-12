c083ea763f3c64d7b2160f8f7adb8c87b2191930
Alatyami <alatyami@gmail.com>
2017-11-11 21:48:55 -0500

    #17 Added Stomach and it drops from vanilla cows.

--- src/main/java/growthcraft/milk/GrowthcraftMilk.java
+++ src/main/java/growthcraft/milk/GrowthcraftMilk.java
@@ -1,8 +1,10 @@
 package growthcraft.milk;
 
+import growthcraft.milk.handlers.EntityDropsHandler;
 import growthcraft.milk.init.GrowthcraftMilkBlocks;
 import growthcraft.milk.init.GrowthcraftMilkItems;
 import growthcraft.milk.proxy.CommonProxy;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -37,6 +39,7 @@ public class GrowthcraftMilk {
 
     @Mod.EventHandler
     public static void postInit(FMLPostInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(new EntityDropsHandler());
     }
 
 
--- src/main/java/growthcraft/milk/init/GrowthcraftMilkItems.java
+++ src/main/java/growthcraft/milk/init/GrowthcraftMilkItems.java
@@ -2,6 +2,7 @@ package growthcraft.milk.init;
 
 import growthcraft.milk.Reference;
 import growthcraft.milk.items.ItemSeedThistle;
+import growthcraft.milk.items.ItemStomach;
 import growthcraft.milk.items.ItemThistle;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
@@ -12,22 +13,27 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 import static growthcraft.core.GrowthcraftCore.tabGrowthcraft;
 
 public class GrowthcraftMilkItems {
+
+    public static Item itemStomach;
     public static Item thistle_seed;
     public static Item thistle;
 
     public static void init() {
         thistle = new ItemThistle("thistle");
         thistle_seed = new ItemSeedThistle("thistle_seed");
+        itemStomach = new ItemStomach("stomach");
     }
 
     public static void register() {
         registerItem(thistle);
         registerItem(thistle_seed);
+        registerItem(itemStomach);
     }
 
     public static void registerRenders() {
         registerRender(thistle);
         registerRender(thistle_seed);
+        registerRender(itemStomach);
     }
 
     public static void registerItem(Item item) {
