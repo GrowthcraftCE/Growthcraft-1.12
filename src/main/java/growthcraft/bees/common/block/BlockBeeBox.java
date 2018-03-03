7e9fc2329a62310991f3523c40ee3600027afe99
alesto <alesto@ALESTO-PC>
2018-03-03 19:54:02 +0100

    Added bee hive rotation

--- src/main/java/growthcraft/bees/common/block/BlockBeeBox.java
+++ src/main/java/growthcraft/bees/common/block/BlockBeeBox.java
@@ -7,12 +7,16 @@ import javax.annotation.Nullable;
 
 import growthcraft.bees.Reference;
 import growthcraft.bees.common.tileentity.TileEntityBeeBox;
+import growthcraft.core.api.utils.BlockFlags;
 import growthcraft.core.common.block.GrowthcraftBlockContainer;
+import growthcraft.core.utils.BlockUtils;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
@@ -66,6 +70,42 @@ public class BlockBeeBox extends GrowthcraftBlockContainer {
     public boolean isFullCube(IBlockState state) {
         return false;
     }
+    
+	@Override
+	public boolean isRotatable(IBlockAccess world, BlockPos pos, EnumFacing side)
+	{
+		return true;
+	}
+	
+	protected void setDefaultDirection(World world, BlockPos pos, IBlockState state)
+	{
+		if (!world.isRemote)
+		{
+			EnumFacing facing = BlockUtils.getDefaultDirection(world, pos, state);
+			world.setBlockState(pos, state.withProperty(TYPE_ROTATION, facing), BlockFlags.UPDATE_AND_SYNC);
+		}
+	}
+	
+	protected EnumFacing setOrientWhenPlacing(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer) {
+		EnumFacing facing = EnumFacing.fromAngle(placer.rotationYaw);
+		worldIn.setBlockState(pos, state.withProperty(TYPE_ROTATION, facing), BlockFlags.UPDATE_AND_SYNC);
+		return facing;
+	}
+	
+	@Override
+	public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state)
+	{
+		super.onBlockAdded(worldIn, pos, state);
+		setDefaultDirection(worldIn, pos, state);
+	}
+	
+	@Override
+	public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack)
+	{
+		super.onBlockPlacedBy(worldIn, pos, state, placer, stack);
+		setOrientWhenPlacing(worldIn, pos, state, placer);
+	}
+	
 	
 	public BlockBeeBox setFlammability(int flam)
 	{
