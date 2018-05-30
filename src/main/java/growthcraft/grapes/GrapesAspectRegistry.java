63901d70de462435dfb2c5fda2382713bc149aa6
Sunconure11 <eyespot7b@aol.com>
2018-05-30 14:57:01 -0400

    Start of thaumcraft support

---
+++ src/main/java/growthcraft/grapes/GrapesAspectRegistry.java
@@ -0,0 +1,17 @@
+package growthcraft.grapes;
+
+import growthcraft.grapes.common.items.ItemGrape;
+import net.minecraft.item.ItemStack;
+import thaumcraft.api.ThaumcraftApi;
+import thaumcraft.api.aspects.Aspect;
+import thaumcraft.api.aspects.AspectList;
+
+/**
+ * Created by Joseph on 5/30/2018.
+ */
+public class GrapesAspectRegistry {
+
+	public static void register() {
+		ThaumcraftApi.registerObjectTag(new ItemStack(ItemGrape), new AspectList().add(Aspect.PLANT, 4).add(Aspect.DESIRE, 4));
+	}
+}
