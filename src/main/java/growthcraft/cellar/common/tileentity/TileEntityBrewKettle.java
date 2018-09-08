f5512d253e74f244e7cea2c182a77c7bcd3a5c28
alesto <alesto@ALESTO-PC>
2018-09-08 11:50:06 +0200

    Steam is now emitting at single points only

--- src/main/java/growthcraft/cellar/common/tileentity/TileEntityBrewKettle.java
+++ src/main/java/growthcraft/cellar/common/tileentity/TileEntityBrewKettle.java
@@ -66,10 +66,11 @@ public class TileEntityBrewKettle extends TileEntityCellarDevice implements ITic
 	private BrewKettle brewKettle = new BrewKettle(this, 0, 1, 2, 0, 1);
 	
 	@SideOnly(Side.CLIENT)
-	private PulseStepper animPulsar = new PulseStepper(10, 0);
+	private PulseStepper animPulsar = new PulseStepper(5, 8);
 	@SideOnly(Side.CLIENT)
 	private SpatialRandom sprand = new SpatialRandom();
-
+	@SideOnly(Side.CLIENT)
+	private Pair<Double, Double> steamEmitPos = null;
 
 	@Override
 	protected FluidTank[] createTanks()
@@ -133,32 +134,44 @@ public class TileEntityBrewKettle extends TileEntityCellarDevice implements ITic
 			brewKettle.update();
 		}
 		else {
-			if (animPulsar.update() == PulseStepper.State.PULSE)
-			{
-				if( hasFluid() && isHeated() && hasLid() ) {
-					Pair<Double, Double> p = sprand.nextCenteredD2();
-					
-					// Normalize by 2-Norm (used for velocity)
-					double horDist = Math.sqrt(p.left*p.left+p.right*p.right);
-					if( Math.abs(horDist) >= 0.00001 ) {
-						p = new Pair<Double, Double>(p.left/horDist, p.right/horDist);
+			if( hasFluid() && isHeated() && hasLid() ) {
+				PulseStepper.State pulseState = animPulsar.update(); 
+				if (pulseState == PulseStepper.State.NONE || steamEmitPos == null ) {
+					// NOTE: steamEmitPos == null means initial state.
+					if( world.rand.nextInt(20) >= 19) {
+						steamEmitPos = sprand.nextCenteredD2();
+						animPulsar.reset();
 					}
-					else {
-						p = new Pair<Double, Double>(1.0, 0.0);
+				}
+				else if (pulseState == PulseStepper.State.PULSE)
+				{
+					if( world.rand.nextInt(10) >= 3) {
+						// Spread steamEmitPos a little
+						Pair<Double, Double> p = sprand.nextCenteredD2();
+						p = new Pair<Double, Double>( steamEmitPos.left+p.left*0.5, steamEmitPos.right+p.right*0.5);
+						
+						// Normalize by 2-Norm (used for velocity)
+						double horDist = Math.sqrt(p.left*p.left+p.right*p.right);
+						if( Math.abs(horDist) >= 0.00001 ) {
+							p = new Pair<Double, Double>(p.left/horDist, p.right/horDist);
+						}
+						else {
+							p = new Pair<Double, Double>(1.0, 0.0);
+						}
+						
+						// Normalize by max-Norm (used for position)
+						double maxHorDist = Math.max(Math.abs(p.left), Math.abs(p.right));
+						Pair<Double, Double> pMax = new Pair<Double, Double>(p.left/maxHorDist, p.right/maxHorDist);
+						
+						double vel = (0.125 + 0.1 * world.rand.nextDouble()) * 0.0625;
+						double px = (double)pos.getX() + 0.5 + pMax.left * 0.5;
+						double py = (double)pos.getY() + 1.0;
+						double pz = (double)pos.getZ() + 0.5 + pMax.right * 0.5;
+						double vx = p.left * vel;
+						double vy = vel;
+						double vz = p.right * vel;
+						FXHelper.emitSmoke(world, px, py, pz, vx, vy, vz);
 					}
-					
-					// Normalize by max-Norm (used for position)
-					double maxHorDist = Math.max(Math.abs(p.left), Math.abs(p.right));
-					Pair<Double, Double> pMax = new Pair<Double, Double>(p.left/maxHorDist, p.right/maxHorDist);
-					
-					double vel = (0.125 + 0.1 * world.rand.nextDouble()) * 0.0625;
-					double px = (double)pos.getX() + 0.5 + pMax.left * 0.5;
-					double py = (double)pos.getY() + 1.0;
-					double pz = (double)pos.getZ() + 0.5 + pMax.right * 0.5;
-					double vx = p.left * vel;
-					double vy = vel;
-					double vz = p.right * vel;
-					FXHelper.emitSmoke(world, px, py, pz, vx, vy, vz);
 				}
 			}
 		}
