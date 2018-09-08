4dc6b82f1edfd6f838b089dff8d1512c2375b906
alesto <alesto@ALESTO-PC>
2018-09-08 12:09:17 +0200

    Refactorings and cleanups ...

--- src/main/java/growthcraft/cellar/common/tileentity/TileEntityBrewKettle.java
+++ src/main/java/growthcraft/cellar/common/tileentity/TileEntityBrewKettle.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import growthcraft.cellar.shared.config.GrowthcraftCellarConfig;
 import growthcraft.cellar.shared.init.GrowthcraftCellarItems;
+import growthcraft.cellar.client.particle.BrewKettleLidSteamEmitter;
 import growthcraft.cellar.common.inventory.ContainerBrewKettle;
 import growthcraft.cellar.common.tileentity.device.BrewKettle;
 import growthcraft.cellar.common.tileentity.fluids.CellarTank;
@@ -66,11 +67,9 @@ public class TileEntityBrewKettle extends TileEntityCellarDevice implements ITic
 	private BrewKettle brewKettle = new BrewKettle(this, 0, 1, 2, 0, 1);
 	
 	@SideOnly(Side.CLIENT)
-	private PulseStepper animPulsar = new PulseStepper(5, 8);
+	private BrewKettleLidSteamEmitter lidSteamEmitter = new BrewKettleLidSteamEmitter();
 	@SideOnly(Side.CLIENT)
 	private SpatialRandom sprand = new SpatialRandom();
-	@SideOnly(Side.CLIENT)
-	private Pair<Double, Double> steamEmitPos = null;
 	@SideOnly(Side.CLIENT)
 	private boolean animLastLid = false;	// NOTE: Only use it inside update() on the client side. 
 
@@ -138,45 +137,7 @@ public class TileEntityBrewKettle extends TileEntityCellarDevice implements ITic
 		else {
 			if( hasFluid() && isHeated() ) {
 				if( hasLid() ) {
-					PulseStepper.State pulseState = animPulsar.update(); 
-					if (pulseState == PulseStepper.State.NONE || steamEmitPos == null ) {
-						// NOTE: steamEmitPos == null means initial state.
-						if( world.rand.nextInt(20) >= 19) {
-							steamEmitPos = sprand.nextCenteredD2();
-							animPulsar.reset();
-						}
-					}
-					else if (pulseState == PulseStepper.State.PULSE)
-					{
-						if( world.rand.nextInt(10) >= 3) {
-							// Spread steamEmitPos a little
-							Pair<Double, Double> p = sprand.nextCenteredD2();
-							p = new Pair<Double, Double>( steamEmitPos.left+p.left*0.5, steamEmitPos.right+p.right*0.5);
-							
-							// Normalize by 2-Norm (used for velocity)
-							double horDist = Math.sqrt(p.left*p.left+p.right*p.right);
-							if( Math.abs(horDist) >= 0.00001 ) {
-								p = new Pair<Double, Double>(p.left/horDist, p.right/horDist);
-							}
-							else {
-								p = new Pair<Double, Double>(1.0, 0.0);
-							}
-							
-							// Normalize by max-Norm (used for position)
-							double maxHorDist = Math.max(Math.abs(p.left), Math.abs(p.right));
-							Pair<Double, Double> pMax = new Pair<Double, Double>(p.left/maxHorDist, p.right/maxHorDist);
-							
-							double vel = (0.125 + 0.1 * world.rand.nextDouble()) * 0.0625;
-							double px = (double)pos.getX() + 0.5 + pMax.left * 0.5;
-							double py = (double)pos.getY() + 1.0;
-							double pz = (double)pos.getZ() + 0.5 + pMax.right * 0.5;
-							double vx = p.left * vel;
-							double vy = vel;
-							double vz = p.right * vel;
-							FXHelper.emitSmoke(world, px, py, pz, vx, vy, vz);
-						}
-					}
-					
+					lidSteamEmitter.update(world, pos);
 					animLastLid = true;
 				}
 				else {
