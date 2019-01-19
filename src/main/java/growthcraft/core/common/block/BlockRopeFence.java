07fd32ac991ad3f8b746cefbdfc46fd1387c1d9f
alesto <alesto@ALESTO-PC>
2019-01-19 13:41:48 +0100

    Single rope fence block has now more volume

--- src/main/java/growthcraft/core/common/block/BlockRopeFence.java
+++ src/main/java/growthcraft/core/common/block/BlockRopeFence.java
@@ -131,12 +131,28 @@ public class BlockRopeFence extends BlockRopeBase {
     @SuppressWarnings("deprecation")
     @Override
     public IBlockState getActualState(IBlockState state, IBlockAccess worldIn, BlockPos pos) {
-        return state.withProperty(NORTH, canConnectRopeTo(worldIn, pos, EnumFacing.NORTH))
-                .withProperty(EAST, canConnectRopeTo(worldIn, pos, EnumFacing.EAST))
-                .withProperty(SOUTH, canConnectRopeTo(worldIn, pos, EnumFacing.SOUTH))
-                .withProperty(WEST, canConnectRopeTo(worldIn, pos, EnumFacing.WEST))
-                .withProperty(UP, canConnectRopeTo(worldIn, pos, EnumFacing.UP))
-                .withProperty(DOWN, canConnectRopeTo(worldIn, pos, EnumFacing.DOWN));
+    	boolean vN = canConnectRopeTo(worldIn, pos, EnumFacing.NORTH);
+    	boolean vE = canConnectRopeTo(worldIn, pos, EnumFacing.EAST);
+    	boolean vS = canConnectRopeTo(worldIn, pos, EnumFacing.SOUTH);
+    	boolean vW = canConnectRopeTo(worldIn, pos, EnumFacing.WEST);
+    	boolean vU = canConnectRopeTo(worldIn, pos, EnumFacing.UP);
+    	boolean vD = canConnectRopeTo(worldIn, pos, EnumFacing.DOWN);
+    	
+    	if( !vN && !vE && !vS && !vW && !vU && !vD ) {
+    		vN = true;
+    		vE = true;
+    		vS = true;
+    		vW = true;
+    		vU = true;
+    		vD = true;
+    	}
+    	
+        return state.withProperty(NORTH, vN)
+                .withProperty(EAST, vE)
+                .withProperty(SOUTH, vS)
+                .withProperty(WEST, vW)
+                .withProperty(UP, vU)
+                .withProperty(DOWN, vD);
     }
 
     @Override
