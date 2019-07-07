dbf45c883095993f10b187e17a8ebdc576502506
alesto <alesto@ALESTO-PC>
2019-07-07 16:44:27 +0200

    Fixed shift click behavior

--- src/main/java/growthcraft/milk/common/item/ItemBlockCheeseBlock.java
+++ src/main/java/growthcraft/milk/common/item/ItemBlockCheeseBlock.java
@@ -1,20 +1,31 @@
 package growthcraft.milk.common.item;
 
+import growthcraft.core.shared.block.GrowthcraftBlockContainer;
 import growthcraft.core.shared.definition.IObjectVariant;
 import growthcraft.core.shared.io.nbt.NBTHelper;
 import growthcraft.core.shared.item.IItemTileBlock;
+import growthcraft.core.shared.tileentity.feature.IItemOperable;
 import growthcraft.milk.GrowthcraftMilk;
+import growthcraft.milk.common.block.BlockCheeseBlock;
+import growthcraft.milk.common.tileentity.TileEntityCheeseBlock;
 import growthcraft.milk.shared.cheese.CheeseIO;
 import growthcraft.milk.shared.cheese.CheeseUtils;
 import growthcraft.milk.shared.definition.EnumCheeseStage;
 import growthcraft.milk.shared.definition.ICheeseBlockStackFactory;
 import growthcraft.milk.shared.definition.ICheeseType;
 import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.NonNullList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -68,6 +79,28 @@ public class ItemBlockCheeseBlock<T extends ICheeseType & IObjectVariant> extend
 		final NBTTagCompound tag = getTileTagCompoundABS(stack);
 		return EnumCheeseStage.loadFromNBT(tag);
 	}
+	
+	@Override
+    public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) { 
+		// Handle shift click behavior
+		IBlockState iblockstate = worldIn.getBlockState(pos);
+        Block block = iblockstate.getBlock();
+        if( block instanceof BlockCheeseBlock ) {
+        	// FIXME: Copied logic from GrowthcraftBlockContainer.handleOnUseItem() . Avoid code duplication!
+        	BlockCheeseBlock cheeseBlock = (BlockCheeseBlock)block;
+        	TileEntityCheeseBlock te = cheeseBlock.getTileEntity(worldIn, pos);
+        	if( te != null ) {
+        		ItemStack onHand = player.getHeldItem(hand);
+        		if( te.tryPlaceItem(IItemOperable.Action.RIGHT, player, onHand) ) {
+        			cheeseBlock.markBlockForUpdate(worldIn, pos);
+        			return EnumActionResult.SUCCESS;
+        		}
+        	}
+        }
+		
+    	return super.onItemUse(player, worldIn, pos, hand, facing, hitX, hitY, hitZ);
+    }
+
 
 	@Override
 	public String getUnlocalizedName(ItemStack stack)
