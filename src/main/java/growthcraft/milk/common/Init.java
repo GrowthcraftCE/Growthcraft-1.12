ce4d2d9ef0742dd8826d6a33619f6b3e34d9e88d
Alatyami <alatyami@gmail.com>
2019-07-18 09:24:42 -0400

    Removed tasking comments for recipes that have already been implemented.

--- src/main/java/growthcraft/milk/common/Init.java
+++ src/main/java/growthcraft/milk/common/Init.java
@@ -912,7 +912,7 @@ public class Init {
 			CheeseVatRecipeBuilder.buildRecipe("CASU MARZU Recipe")
 					.outputFluids(AgedCheeseTypes.CASU_MARZU.getFluids().asFluidStack(5000))
 					.inputFluids(new TaggedFluidStacks(5000, "milk_curds"))
-					.inputItems(new OreItemStacks(saltOre, 1), new OreItemStacks("listAllfruit", 1), new ItemStack(Items.ROTTEN_FLESH))
+					.inputItems(new OreItemStacks(saltOre, 1), new ItemStack(Items.ROTTEN_FLESH))
 					.register();
 		}
 	}
@@ -932,12 +932,6 @@ public class Init {
 				Ingredient.fromItem(Items.BOWL)
 		);
 
-		// TODO: Register standard shapeless and shaped recipes.
-		//			- Cheese Vat recipe
-		//			- Churn recipe
-		//			- Cheese Press recipe
-		//			- Pancheon recipe
-
 		// TODO: Register non-standard Cheese Vat recipes.
 		//			- Cheese
 
@@ -945,151 +939,6 @@ public class Init {
 		//			- Cream recipe
 		//			- Skim Milk recipe
 
-
-/*		
-
-		GameRegistry.addRecipe(new ShapelessOreRecipe(GrowthcraftMilkBlocks.cheeseVat.asStack(),
-			GrowthcraftCellarBlocks.brewKettle.asStack()
-		));
-
-		GameRegistry.addRecipe(new ShapedOreRecipe(GrowthcraftMilkBlocks.churn.asStack(),
-			" S ",
-			"P P",
-			"PPP",
-			'S', "stickWood",
-			'P', "plankWood"
-		));
-
-		GameRegistry.addRecipe(new ShapedOreRecipe(GrowthcraftMilkBlocks.cheesePress.asStack(),
-			"iii",
-			"iCi",
-			"ppp",
-			'i', "ingotIron",
-			'C', Blocks.CHEST,
-			'p', "slabWood"
-		));
-
-		GameRegistry.addRecipe(new ShapedOreRecipe(GrowthcraftMilkBlocks.pancheon.asStack(),
-			"c c",
-			"ccc",
-			'c', Items.CLAY_BALL
-		));
-		
-		// Salted Butter
-		GameRegistry.addRecipe(new ShapelessOreRecipe(ButterTypes.SALTED.asStack(), ButterTypes.UNSALTED.asStack(), "foodSalt"));
-
-		// Yogurt - Plain
-		GameRegistry.addRecipe(new ShapelessOreRecipe(YogurtTypes.PLAIN.asStack(),
-			Items.MILK_BUCKET,
-			GrowthcraftMilkItems.starterCulture.asStack(),
-			Items.BOWL
-		));
-
-		// Yogurt - Chocolate
-		GameRegistry.addRecipe(new ShapelessOreRecipe(YogurtTypes.CHOCOLATE.asStack(),
-			Items.MILK_BUCKET,
-			GrowthcraftMilkItems.starterCulture.asStack(),
-			Items.BOWL,
-			EnumDye.COCOA_BEANS.asStack()
-		));
-
-		// Yogurt - Grape
-		GameRegistry.addRecipe(new ShapelessOreRecipe(YogurtTypes.GRAPEPURPLE.asStack(),
-			Items.MILK_BUCKET,
-			GrowthcraftMilkItems.starterCulture.asStack(),
-			Items.BOWL,
-			"foodGrapesPurple"
-		));
-
-		// Yogurt - Apple
-		GameRegistry.addRecipe(new ShapelessOreRecipe(YogurtTypes.APPLE.asStack(),
-			Items.MILK_BUCKET,
-			GrowthcraftMilkItems.starterCulture.asStack(),
-			Items.BOWL,
-			"foodApple"
-		));
-
-		// Yogurt - Honey
-		GameRegistry.addRecipe(new ShapelessOreRecipe(YogurtTypes.HONEY.asStack(),
-			Items.MILK_BUCKET,
-			GrowthcraftMilkItems.starterCulture.asStack(),
-			Items.BOWL,
-			"honeyDrop"
-		));
-
-		// Yogurt - Melon
-		GameRegistry.addRecipe(new ShapelessOreRecipe(YogurtTypes.WATERMELON.asStack(),
-			Items.MILK_BUCKET,
-			GrowthcraftMilkItems.starterCulture.asStack(),
-			Items.BOWL,
-			"foodMelon"
-		));
-
-		// Ice Cream - Plain
-		GameRegistry.addRecipe(new ShapelessOreRecipe(IceCreamTypes.PLAIN.asStack(),
-			GrowthcraftMilkFluids.cream.asBucketItemStack(),
-			Items.MILK_BUCKET,
-			Items.BOWL,
-			Items.SUGAR
-		));
-
-		// Ice Cream - Chocolate
-		GameRegistry.addRecipe(new ShapelessOreRecipe(IceCreamTypes.CHOCOLATE.asStack(),
-			GrowthcraftMilkFluids.cream.asBucketItemStack(),
-			Items.MILK_BUCKET,
-			Items.BOWL,
-			Items.SUGAR,
-			EnumDye.COCOA_BEANS.asStack()
-		));
-
-		// Ice Cream - Grape
-		GameRegistry.addRecipe(new ShapelessOreRecipe(IceCreamTypes.GRAPEPURPLE.asStack(),
-			GrowthcraftMilkFluids.cream.asBucketItemStack(),
-			Items.MILK_BUCKET,
-			Items.BOWL,
-			Items.SUGAR,
-			"foodGrapesPurple"
-		));
-
-		// Ice Cream - Apple
-		GameRegistry.addRecipe(new ShapelessOreRecipe(IceCreamTypes.APPLE.asStack(),
-			GrowthcraftMilkFluids.cream.asBucketItemStack(),
-			Items.MILK_BUCKET,
-			Items.BOWL,
-			Items.SUGAR,
-			"foodApple"
-		));
-
-		// Ice Cream - Honey
-		GameRegistry.addRecipe(new ShapelessOreRecipe(IceCreamTypes.HONEY.asStack(),
-			GrowthcraftMilkFluids.cream.asBucketItemStack(),
-			Items.MILK_BUCKET,
-			Items.BOWL,
-			Items.SUGAR,
-			"honeyDrop"
-		));
-
-		// Ice Cream - Melon
-		GameRegistry.addRecipe(new ShapelessOreRecipe(IceCreamTypes.WATERMELON.asStack(),
-			GrowthcraftMilkFluids.cream.asBucketItemStack(),
-			Items.MILK_BUCKET,
-			Items.BOWL,
-			Items.SUGAR,
-			"foodMelon"
-		));
-
-		// Cheese Cloth
-		GameRegistry.addRecipe(new ShapedOreRecipe(GrowthcraftMilkItems.cheeseCloth.asStack(4),
-			"sss",
-			"s s",
-			"sss",
-			's', Items.STRING
-		));
-		
-		if (GrowthcraftMilkItems.thistleSeed != null && GrowthcraftMilkBlocks.thistle != null)
-		{
-			GameRegistry.addShapelessRecipe(GrowthcraftMilkItems.thistleSeed.asStack(2), GrowthcraftMilkItems.thistle.getItem());
-		} */
 	}
 	
 	private static <E extends Enum<?> & ICheeseType & IStringSerializable> void registerCheesePressRecipesFor(Class<E> enumType) {
