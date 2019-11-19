030197cfa762ead400f662c360d16aca1b04968c
Alatyami <alatyami@gmail.com>
2019-11-18 23:43:46 -0500

    Progress #54 Added beginings of JEI Custom device recipes

--- src/main/java/growthcraft/cellar/shared/Reference.java
+++ src/main/java/growthcraft/cellar/shared/Reference.java
@@ -4,4 +4,9 @@ public class Reference {
     public static final String MODID = growthcraft.core.shared.Reference.MODID + "_cellar";
     public static final String NAME = "Growthcraft Cellar";
     public static final String VERSION = growthcraft.core.shared.Reference.VERSION;
+
+    public static class JEI {
+        public static final String FERMENTATION_BARREL_UID = MODID + ":fermentationBarrelCrafting";
+        public static final String FERMENTATION_BARREL_TITLE = "jei.recipe.fermenationBarrel";
+    }
 }
