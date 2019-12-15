b6ded582cdd1eda16434552d295873f87a3d0375
alesto <alesto@ALESTO-PC>
2019-12-15 13:48:28 +0100

    Improved pancheon block updates

--- src/main/java/growthcraft/milk/common/tileentity/device/Pancheon.java
+++ src/main/java/growthcraft/milk/common/tileentity/device/Pancheon.java
@@ -66,7 +66,6 @@ public class Pancheon extends DeviceProgressive {
 
             final FluidStack bottom = recipe.getBottomOutputFluid();
             if (bottom != null) this.bottomSlot.fill(bottom, true);
-            markDirtyAndUpdate(true);
         }
     }
 
@@ -82,8 +81,10 @@ public class Pancheon extends DeviceProgressive {
                 resetTime();
                 commitRecipe();
             }
+            markDirtyAndUpdate(true);
         } else {
-            if (resetTime()) markDirty();
+            if (resetTime())
+            	markDirtyAndUpdate(false);
         }
     }
 }
