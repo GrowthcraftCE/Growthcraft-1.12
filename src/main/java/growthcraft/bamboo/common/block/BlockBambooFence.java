de0a080889945a22e60a0e17088a3d3d3bcb59ce
Doenerstyle <103001779+Doenerstyle@users.noreply.github.com>
2025-08-30 14:22:14 +0200

    Fixed Apple Fence and Bamboo Fence (as well as Gates) using Stone sounds

--- src/main/java/growthcraft/bamboo/common/block/BlockBambooFence.java
+++ src/main/java/growthcraft/bamboo/common/block/BlockBambooFence.java
@@ -2,6 +2,7 @@ package growthcraft.bamboo.common.block;
 
 import growthcraft.bamboo.shared.Reference;
 import net.minecraft.block.BlockFence;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.util.ResourceLocation;
@@ -13,6 +14,7 @@ public class BlockBambooFence extends BlockFence {
         this.setTranslationKey(unlocalizedName);
         this.setRegistryName(new ResourceLocation(Reference.MODID, unlocalizedName));
         this.setHardness(2.0F);
+        this.setSoundType(SoundType.WOOD);
         this.setHarvestLevel("axe", 1);
         this.setResistance(5.0F);
         this.useNeighborBrightness = true;
